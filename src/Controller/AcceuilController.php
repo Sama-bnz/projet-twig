743058844e0f891ed97bc53f580cdf4ea2a50f42
Sama <thomasbenmezded@gmail.com>
2022-06-30 13:56:03 +0200

    projet-twig

---
+++ src/Controller/AcceuilController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class AcceuilController
+{
+/**
+ * @Route ("/bonjour", name="bonjour")
+ */
+    public function bonjour()
+    {
+    return new Response('Bonjour');
+    }
+}
