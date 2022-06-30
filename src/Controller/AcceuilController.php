f7bf906894358eba794da1a0523660c309f23397
Sama <thomasbenmezded@gmail.com>
2022-06-30 16:37:43 +0200

    cordialement

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -1,26 +1,35 @@
 <?php
 
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class AcceuilController extends AbstractController
 {
-/**
- * @Route ("/bonjour", name="bonjour")
- */
+    /**
+     * @Route ("/bonjour", name="bonjour")
+     */
     public function bonjour()
     {
         //Je crée ma variable name
-        $name = 'Thomas';
+        $category =[
+            'title' =>'Politique',
+            'color' => 'red',
+            'published' => true,
+            'description'=> "Decrypter l'actualité politique avec le prisme de David Robert"
+        ];
+
+
 
 
         // je récupére le fichier twig
         // Je transforme en fichier html
         // et la retourne au navigateur
 
-        return $this->render('show_name.html.twig',['name' => $name]);
+        return $this->render('show_name.html.twig', ['category'=> $category]);
 
     }
 }
