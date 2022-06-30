61e901172bb8a8ae22f6d5328e1817d3cc0c43c6
Sama <thomasbenmezded@gmail.com>
2022-06-30 15:43:21 +0200

    update-twig

--- src/Controller/AcceuilController.php
+++ src/Controller/AcceuilController.php
@@ -5,13 +5,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class AcceuilController
+class AcceuilController extends AbstractController
 {
 /**
  * @Route ("/bonjour", name="bonjour")
  */
     public function bonjour()
     {
-    return new Response('Bonjour');
+        //Je crée ma variable name
+        $name = 'Thomas';
+
+
+        // je récupére le fichier twig
+        // Je transforme en fichier html
+        // et la retourne au navigateur
+
+        return $this->render('show_name.html.twig',['name' => $name]);
+
     }
 }
